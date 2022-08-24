0c993cee3f3010a6e3816ed8fca8bb337121ae1e
pankajm333 <111896613+pankajm333@users.noreply.github.com>
2022-08-24 15:58:30 +0530

    Add files via upload

--- Game.py
+++ Game.py
@@ -1,7 +1,12 @@
 import pygame
 import random
+import os
+
+pygame.mixer.init()
 
 pygame.init()
+
+
 
 # Colors
 white = (255, 255, 255)
@@ -13,11 +18,17 @@ screen_width = 900
 screen_height = 600
 gameWindow = pygame.display.set_mode((screen_width, screen_height))
 
+#Background Image
+bgimg = pygame.image.load("SnakePos.jpg")
+bgimg = pygame.transform.scale(bgimg, (screen_width, screen_height)).convert_alpha()
+
+
 # Game Title
-pygame.display.set_caption("SnakesWithHarry")
+pygame.display.set_caption("SnakesWithPankaj")
 pygame.display.update()
 clock = pygame.time.Clock()
 font = pygame.font.SysFont(None, 55)
+
 
 def text_screen(text, color, x, y):
     screen_text = font.render(text, True, color)
@@ -27,6 +38,25 @@ def text_screen(text, color, x, y):
 def plot_snake(gameWindow, color, snk_list, snake_size):
     for x,y in snk_list:
         pygame.draw.rect(gameWindow, color, [x, y, snake_size, snake_size])
+
+def welcome():
+    exit_game = False
+    while not exit_game:
+        gameWindow.fill((233,210,229))
+        text_screen("Welcome to Snakes", black, 260, 250)
+        text_screen("Press Space Bar To Play", black, 232, 290)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_game = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    pygame.mixer.music.load('bag.mp3')
+                    pygame.mixer.music.play()
+                    gameloop()
+
+        pygame.display.update()
+        clock.tick(60)
+
 
 # Game Loop
 def gameloop():
@@ -39,6 +69,11 @@ def gameloop():
     velocity_y = 0
     snk_list = []
     snk_length = 1
+    # Check if hiscore file exists
+    if(not os.path.exists("hiscore.txt")):
+        with open("hiscore.txt", "w") as f:
+            f.write("0")
+
     with open("hiscore.txt", "r") as f:
         hiscore = f.read()
 
@@ -50,6 +85,8 @@ def gameloop():
     fps = 60
     while not exit_game:
         if game_over:
+            with open("hiscore.txt", "w") as f:
+                f.write(str(hiscore))
             gameWindow.fill(white)
             text_screen("Game Over! Press Enter To Continue", red, 100, 250)
 
@@ -59,7 +96,7 @@ def gameloop():
 
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN:
-                        gameloop()
+                        welcome()
 
         else:
 
@@ -84,6 +121,9 @@ def gameloop():
                         velocity_y = init_velocity
                         velocity_x = 0
 
+                    if event.key == pygame.K_q:
+                        score +=10
+
             snake_x = snake_x + velocity_x
             snake_y = snake_y + velocity_y
 
@@ -92,12 +132,12 @@ def gameloop():
                 food_x = random.randint(20, screen_width / 2)
                 food_y = random.randint(20, screen_height / 2)
                 snk_length +=5
-                if score > int(hiscore):
+                if score>int(hiscore):
                     hiscore = score
 
             gameWindow.fill(white)
-           #     text_screen("Score: " + str(score *10), red, 5, 5)
-            text_screen("Score: " + str(score) + " Hiscore:  "+str(hiscore), red, 5, 5)
+            gameWindow.blit(bgimg, (0, 0))
+            text_screen("Score: " + str(score) + "  Hiscore: "+str(hiscore), red, 5, 5)
             pygame.draw.rect(gameWindow, red, [food_x, food_y, snake_size, snake_size])
 
 
@@ -111,14 +151,17 @@ def gameloop():
 
             if head in snk_list[:-1]:
                 game_over = True
+                pygame.mixer.music.load('gameOver.mp3')
+                pygame.mixer.music.play()
 
             if snake_x<0 or snake_x>screen_width or snake_y<0 or snake_y>screen_height:
                 game_over = True
+                pygame.mixer.music.load('gameOver.mp3')
+                pygame.mixer.music.play()
             plot_snake(gameWindow, black, snk_list, snake_size)
         pygame.display.update()
         clock.tick(fps)
 
     pygame.quit()
     quit()
-gameloop()
-
+welcome()
